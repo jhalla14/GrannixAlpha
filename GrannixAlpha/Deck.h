bd087e647d3e97b9b952426c1ef68ca8385ff325
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2013-07-03 23:12:15 -0500

    Created Card class

---
+++ GrannixAlpha/Deck.h
@@ -0,0 +1,13 @@
+//
+//  Deck.h
+//  GrannixAlpha
+//
+//  Created by Joshua Hall  on 7/3/13.
+//  Copyright (c) 2013 Grannix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Deck : NSObject
+
+@end
---
+++ GrannixAlpha/Deck.m
@@ -0,0 +1,13 @@
+//
+//  Deck.m
+//  GrannixAlpha
+//
+//  Created by Joshua Hall  on 7/3/13.
+//  Copyright (c) 2013 Grannix. All rights reserved.
+//
+
+#import "Deck.h"
+
+@implementation Deck
+
+@end
