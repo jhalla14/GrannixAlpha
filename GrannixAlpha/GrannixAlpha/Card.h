fdde5117841301944f604c16e19ca9d14e977b2a
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2013-07-03 23:15:48 -0500

    moved deck and card classes to inside GrannixAlpha folder

---
+++ GrannixAlpha/GrannixAlpha/Card.h
@@ -0,0 +1,16 @@
+//
+//  Card.h
+//  GrannixAlpha
+//
+//  Created by Joshua Hall  on 7/3/13.
+//  Copyright (c) 2013 Grannix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@property (strong, nonatomic) NSString *name;
+@property(strong, nonatomic) NSString *sport;
+
+@end
